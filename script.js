a0e83c91051880fc58c5222a3186a3082bd8ea1c
mukeshkanna11 <agriculturistmukesh11@gmail.com>
2024-09-22 14:34:03 +0530

    Final commit

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
 //Dom js functions//
 
-const emojis =['😊','😊','❤️','❤️','😍','😍','👍','👍','✌️','✌️','😎','😎','😉','😉','⛷️','⛷️']
+const emojis = ['😊', '😊', '❤️', '❤️', '😍', '😍', '👍', '👍', '✌️', '✌️', '😎', '😎', '😉', '😉', '⛷️', '⛷️'];
 
-var   shuf_emojis= emojis.sort(() =>(Math.random()>.5)? 2 :-1);
-for ( var i=0; i<emojis.length; i++){
-    let box=document.createElement('div')
+// Shuffle emojis
+var shuf_emojis = emojis.sort(() => (Math.random() > 0.5) ? 1 : -1);
+
+// Create game board
+for (var i = 0; i < emojis.length; i++) {
+    let box = document.createElement('div');
     box.className = 'item';
-    box.innerHTML = shuf_emojis[i]
+    box.innerHTML = shuf_emojis[i];
 
-    box.onclick = function(){
-        this.classList.add('boxopen')
-        setTimeout(function(){
-          if(document.querySelectorAll('.boxopen').length > 1 ){
-            if(document.querySelectorAll('.boxopen')[0].innerHTML == 
-            document.querySelectorAll('.boxopen')[1].innerHTML){
-                document.querySelectorAll('.boxopen')[0].classList.add
-                ('.boxmatch')
-                document.querySelectorAll('.boxopen')[1].classList.add
-                ('.boxmatch')
-
-                document.querySelectorAll('.boxopen')[1].classList.remove
-                ('.boxopen')
-                document.querySelectorAll('.boxopen')[0].classList.remove
-                ('.boxopen')
-
-                if(document.querySelectorAll('.boxmatch').length == emojis.
-                length){
-                    alert('Win')
-                }
-            
-            }else{
-                document.querySelectorAll('.boxopen')[1].classList.remove
-                ('.boxopen')
-                document.querySelectorAll('.boxopen')[0].classList.remove
-                ('.boxopen')
-
-            }
-          }
-        },500)
+    box.onclick = function () {
         
-    }
+        if (this.classList.contains('boxopen') || this.classList.contains('boxmatch')) {
+            return;
+        }
+        
+        this.classList.add('boxopen');
+        
+        setTimeout(function () {
+            const openBoxes = document.querySelectorAll('.boxopen');
+
+            if (openBoxes.length > 1) {
+                if (openBoxes[0].innerHTML === openBoxes[1].innerHTML) {
+                    // Add the 'boxmatch' class to both matched boxes
+                    openBoxes[0].classList.add('boxmatch');
+                    openBoxes[1].classList.add('boxmatch');
+
+                   
+                    openBoxes[0].classList.remove('boxopen');
+                    openBoxes[1].classList.remove('boxopen');
+
+                   
+                    if (document.querySelectorAll('.boxmatch').length === emojis.length) {
+                        setTimeout(() => {
+                            alert('Congratulations! You win!');
+                        }, 200); // Delay for smooth transition
+                    }
+                } else {
+                    
+                    setTimeout(() => {
+                        openBoxes[0].classList.remove('boxopen');
+                        openBoxes[1].classList.remove('boxopen');
+                    }, 500);
+                }
+            }
+        }, 500);
+    };
 
     document.querySelector('.game').appendChild(box);
-
 }
